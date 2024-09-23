a5d230a2f954960ecdd580826e51ee65c9aa9e8f
Your Name <you@example.com>
2024-09-23 15:58:34 +0800

    change js

--- _class/docsify/docsify-mermaid-zoom/_js/main.js
+++
@@ -1,7 +0,0 @@
-import { plugin } from "./docsify-mermaid-zoom.js";
-
-const { mermaidZoom = {}, mermaidConfig = {} } = window.$docsify;
-
-mermaidConfig.postRenderCallback = plugin(...Object.values(mermaidZoom));
-
-window.$docsify.mermaidConfig = mermaidConfig;
